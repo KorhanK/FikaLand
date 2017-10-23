e6f7f6f9e7ff5c7f48a10a3aad7216c978ce2b0b
Korhan Kutlu <korhankutlu@gmail.com>
2017-10-23 15:53:59 +0200

    game and house

--- src/Game.java
+++ src/Game.java
@@ -1,10 +1,13 @@
 
 public class Game {
-
+	private static boolean running = true; 
 	
 	public static void main(String[] args) {
 		Player player = new Player(10000, 10, 5, false, false, 0, null, null, args, 0, null, null);
-
+		while(running){
+			
+			running = false;
+		}
 	}
 
 }
