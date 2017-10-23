e36d36f60311828245cbd22cfff6f87d1ccd30e3
Korhan Kutlu <korhankutlu@gmail.com>
2017-10-23 14:10:53 +0200

    Test Test

---
+++ src/Game.java
@@ -0,0 +1,10 @@
+
+public class Game {
+
+	
+	public static void main(String[] args) {
+		Player player = new Player(10000, 10, 5, false, false, 0, null, null, args, 0, null, null);
+
+	}
+
+}
---
+++ src/Player.java
@@ -0,0 +1,37 @@
+
+public class Player {
+	private int money;
+	private int morale;
+	private int luck;
+	private boolean knowsEnglish;
+	private boolean isMarried;
+	private int numberOfChildren;
+	private Job job;
+	private Education education;
+	private String[] friends;
+	private int score;
+	private House house;
+	private Swedish swedishLevel;
+	public Player(int money, int morale, int luck, boolean knowsEnglish,
+			boolean isMarried, int numberOfChildren, Job job,
+			Education education, String[] friends, int score, House house,
+			Swedish swedishLevel) {
+		super();
+		this.money = money;
+		this.morale = morale;
+		this.luck = luck;
+		this.knowsEnglish = knowsEnglish;
+		this.isMarried = isMarried;
+		this.numberOfChildren = numberOfChildren;
+		this.job = job;
+		this.education = education;
+		this.friends = friends;
+		this.score = score;
+		this.house = house;
+		this.swedishLevel = swedishLevel;
+	}
+	
+	
+	
+
+}
